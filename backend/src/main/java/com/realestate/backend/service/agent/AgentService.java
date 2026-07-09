dda989f723b986895f98659ceeabd8373e42fac4
elsen111 <csstudent1111@gmail.com>
2026-07-09 22:30:25 +0300

    feat(property): implement agent assigned properties feature

--- backend/src/main/java/com/realestate/backend/service/agent/AgentService.java
+++ backend/src/main/java/com/realestate/backend/service/agent/AgentService.java
@@ -1,9 +1,20 @@
 package com.realestate.backend.service.agent;
 
 import com.realestate.backend.dto.agent.response.AgentResponse;
+import com.realestate.backend.dto.property.request.PropertyFilterRequest;
+import com.realestate.backend.dto.property.response.PropertyResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.UUID;
 
 public interface AgentService {
+
     AgentResponse getAgentByUserId(UUID userId);
+
+    Page<PropertyResponse> getAgentProperties(
+            UUID  userId,
+            PropertyFilterRequest filter,
+            Pageable pageable
+    );
 }
--- backend/src/main/java/com/realestate/backend/service/agent/AgentServiceImpl.java
+++ backend/src/main/java/com/realestate/backend/service/agent/AgentServiceImpl.java
@@ -2,11 +2,21 @@ package com.realestate.backend.service.agent;
 
 
 import com.realestate.backend.dto.agent.response.AgentResponse;
+import com.realestate.backend.dto.property.request.PropertyFilterRequest;
+import com.realestate.backend.dto.property.response.PropertyResponse;
 import com.realestate.backend.entity.AgencyMemberEntity;
+import com.realestate.backend.entity.PropertyEntity;
 import com.realestate.backend.exception.ResourceNotFoundException;
+import com.realestate.backend.mapper.property.PropertyMapper;
 import com.realestate.backend.mapper.user.UserMapper;
+import com.realestate.backend.repository.PropertyRepository;
 import com.realestate.backend.repository.UserRepository;
+import com.realestate.backend.repository.specification.AgencyPropertySpecification;
+import com.realestate.backend.repository.specification.PropertySpecification;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
@@ -19,6 +29,9 @@ public class AgentServiceImpl implements AgentService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
 
+    private final PropertyRepository propertyRepository;
+    private final PropertyMapper propertyMapper;
+
     @Override
     public AgentResponse getAgentByUserId(UUID userId) {
 
@@ -27,5 +40,18 @@ public class AgentServiceImpl implements AgentService {
 
         return userMapper.toAgentWithUserIdResponse(member);
 
+    }
+
+    @Override
+    public Page<PropertyResponse> getAgentProperties(UUID userId, PropertyFilterRequest filter, Pageable pageable) {
+
+        userRepository.findAgentMemberByUserId(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("Agent not found with user id: " + userId));
+
+        Specification<PropertyEntity> specification = PropertySpecification
+                .hasAssignedAgentId(userId);
+
+        return propertyRepository.findAll(specification, pageable)
+                .map(propertyMapper::toPublicAgencyPropertyResponse);
     }
 }
