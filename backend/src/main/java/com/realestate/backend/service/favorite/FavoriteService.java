dbb10fd5308ab0a02b6c3da9038ae4128e88813c
elsen111 <csstudent1111@gmail.com>
2026-07-14 19:16:06 +0300

    feat(favorite): implement add property to favorites endpoint

---
+++ backend/src/main/java/com/realestate/backend/service/favorite/FavoriteService.java
@@ -0,0 +1,12 @@
+package com.realestate.backend.service.favorite;
+
+import com.realestate.backend.dto.favorite.response.FavoriteResponse;
+import com.realestate.backend.security.CustomUserDetails;
+
+import java.util.UUID;
+
+public interface FavoriteService {
+
+    FavoriteResponse addFavorite(UUID propertyId, CustomUserDetails currentUser);
+
+}
---
+++ backend/src/main/java/com/realestate/backend/service/favorite/FavoriteServiceImpl.java
@@ -0,0 +1,67 @@
+package com.realestate.backend.service.favorite;
+
+import com.realestate.backend.dto.favorite.response.FavoriteResponse;
+import com.realestate.backend.entity.FavoriteEntity;
+import com.realestate.backend.entity.PropertyEntity;
+import com.realestate.backend.entity.UserEntity;
+import com.realestate.backend.enums.PropertyStatus;
+import com.realestate.backend.exception.BadRequestException;
+import com.realestate.backend.exception.BusinessException;
+import com.realestate.backend.exception.ConflictException;
+import com.realestate.backend.exception.ResourceNotFoundException;
+import com.realestate.backend.mapper.favorite.FavoriteMapper;
+import com.realestate.backend.repository.FavoriteRepository;
+import com.realestate.backend.repository.PropertyRepository;
+import com.realestate.backend.repository.UserRepository;
+import com.realestate.backend.security.CustomUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class FavoriteServiceImpl implements FavoriteService {
+
+    private final UserRepository userRepository;
+    private final FavoriteRepository favoriteRepository;
+    private final FavoriteMapper favoriteMapper;
+    private final PropertyRepository propertyRepository;
+
+    @Override
+    @Transactional
+    public FavoriteResponse addFavorite(UUID propertyId, CustomUserDetails currentUser) {
+
+        if(currentUser == null) {
+            throw new BusinessException("You need to be logged in");
+        }
+
+        UserEntity user = userRepository.findById(currentUser.getId())
+                .orElseThrow(
+                        () -> new ResourceNotFoundException("User not found with id " +  currentUser.getId())
+                );
+
+        boolean IsAlreadyFavorite = favoriteRepository.existsByUser_IdAndProperty_Id(user.getId(), propertyId);
+
+        if(IsAlreadyFavorite) {
+            throw new ConflictException("Property already exists in favorites list.");
+        }
+
+        PropertyEntity property = propertyRepository.getReferenceById(propertyId);
+
+        if(property.getStatus() != PropertyStatus.ACTIVE) {
+            throw  new BadRequestException("Property is not active.");
+        }
+
+        FavoriteEntity addedFavorite = FavoriteEntity.builder()
+                .user(user)
+                .property(property)
+                .build();
+
+        FavoriteEntity savedFavorite = favoriteRepository.saveAndFlush(addedFavorite);
+
+        return favoriteMapper.toCreateFavoriteResponse(savedFavorite);
+
+    }
+}
